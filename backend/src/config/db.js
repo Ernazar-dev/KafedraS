523850b19c20f3965e9967c5e9d86ae85ad1e178
Ernazar <saparniyazovernazar1811@gmail.com>
2026-05-28 18:32:34 +0500

    Db update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,16 +3,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+export const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Neon uchun SSL talab qilinadi
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false,
+      }
+    );
 
 export const connectDB = async () => {
   try {
